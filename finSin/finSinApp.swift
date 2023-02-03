165fa8eb5e6207667d22a31845b85ee57bc59379
Tim Stack <tstack43215@yahoo.com>
2023-02-03 13:20:00 -0600

    Initial Commit

---
+++ finSin/finSinApp.swift
@@ -0,0 +1,17 @@
+//
+//  finSinApp.swift
+//  finSin
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct finSinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
